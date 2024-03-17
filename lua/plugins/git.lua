492446e4c16c51254b79f6c542016d3e87d66183
Mykola Halkin <mykola.halkin@nixs.com>
2024-03-17 14:50:15 +0100

    git support

---
+++ lua/plugins/git.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      vim.g.lazygit_use_neovim_remote = 0
+    end,
+    keys = {
+      { '<leader>gg', ':LazyGit<CR>', desc = 'Join Toggle' },
+    },
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {}
+      vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+      vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
+    end,
+  },
+}
