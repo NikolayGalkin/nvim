e882be348d2879daa614c4f4023343623bc8b926
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-02 16:20:59 +0200

    lsp(signs)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,6 +33,12 @@ return {
       ensure_installed = { "luacheck", "stylua", "ruff", "mypy" },
     })
 
+    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
     local lsp = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
