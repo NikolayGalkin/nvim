5f739165bce706f5b45919e554019864ba8fafde
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-02 18:05:59 +0200

    trouble(diagnostics)

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,8 @@
 return {
   "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {},
   cmd = "Trouble",
+  keys = {
+    { "<leader>td", ":Trouble diagnostics<cr>", desc = "Trouble Diagnostics" },
+  },
 }
