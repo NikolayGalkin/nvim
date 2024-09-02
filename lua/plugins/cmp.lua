9137a171d95a56171be9d6aeaca4ef136690a211
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-02 16:20:45 +0200

    cmp(snippets, path)

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
-    -- 'hrsh7th/cmp-path',
+    "hrsh7th/cmp-path",
     -- 'hrsh7th/cmp-cmdline',
 
     "L3MON4D3/LuaSnip",
@@ -29,28 +29,47 @@ return {
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
-        ["<tab>"] = cmp.mapping.select_next_item(),
-        ["<s-tab>"] = cmp.mapping.select_prev_item(),
+        -- ["<tab>"] = cmp.mapping.select_next_item(),
+        -- ["<s-tab>"] = cmp.mapping.select_prev_item(),
         ["<c-j>"] = cmp.mapping.select_next_item(),
         ["<c-k>"] = cmp.mapping.select_prev_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<s-tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.expand_or_locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" }, -- For luasnip users.
+        { name = "path" },
       }, {
         { name = "buffer" },
       }),
       formatting = {
         format = lspkind.cmp_format({
           mode = "symbol", -- show only symbol annotations
-          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-          ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          maxwidth = 50,
+          ellipsis_char = "...",
+          show_labelDetails = true,
 
           before = function(_, vim_item)
             return vim_item
