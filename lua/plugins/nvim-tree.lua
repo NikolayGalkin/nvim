971ed0fe0aee8e2456da17b0a8138c2b1b276ad9
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-02 16:22:01 +0200

    tree(width, markers)

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,7 +1,18 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-  opts = {},
+  opts = {
+    view = {
+      width = 35,
+      side = "left",
+      relativenumber = true,
+    },
+    renderer = {
+      indent_markers = {
+        enable = true,
+      },
+    },
+  },
   init = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
