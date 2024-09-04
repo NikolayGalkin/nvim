67c50fec47505cc02bacae3f6c2ef469f7990a12
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-04 14:36:47 +0200

    fix(treesitter+html)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
   build = ":TSUpdate",
   config = function(_, _)
     require("nvim-treesitter.configs").setup({
+      ensure_installed = { "html" },
       auto_install = true,
       highlight = {
         enable = true,
