14c1e9415f30760f397529e30dc6846e597d57cc
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-05 13:19:08 +0200

    imp(lua, +jit)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -78,6 +78,25 @@ return {
     lsp.lua_ls.setup({
       on_attach = on_attach,
       capabilities = capabilities,
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            version = "LuaJIT",
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+            },
+          },
+        })
+      end,
       settings = {
         Lua = {
           hint = {
