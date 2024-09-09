a968421a30df6f27d2b95cb3b041d990852a7a4d
Mykola Halkin <mykola.halkin@nixs.com>
2024-09-09 10:03:39 +0200

    feat(bufferline, +tabs_ordinal)

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -11,11 +11,18 @@ return {
           local icon = level:match("error") and " " or " "
           return " " .. icon .. count
         end,
+        numbers = "ordinal",
         show_buffer_close_icons = false,
         show_close_icon = false,
         show_tab_indicators = false,
         style_preset = bufferline.style_preset.no_italic,
       },
     })
+    vim.keymap.set("n", "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>")
+    vim.keymap.set("n", "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>")
+    vim.keymap.set("n", "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>")
+    vim.keymap.set("n", "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>")
+    vim.keymap.set("n", "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>")
+    vim.keymap.set("n", "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>")
   end,
 }
